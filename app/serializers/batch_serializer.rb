4b35638ffafe0e90675a95fb715a31ef50e6b2f3
Akash Srivastava <jaspreet.hd@gmail.com>
2016-01-07 17:04:12 +0530

    created batch serializer

--- app/serializers/batch_serializer.rb
+++ app/serializers/batch_serializer.rb
@@ -2,4 +2,10 @@ class BatchSerializer < ActiveModel::Serializer
   attributes :id,:section,:class_alias
   belongs_to :teacher
   belongs_to :grade
+  class TeacherSerializer < ActiveModel::Serializer
+    attribute :teacher_name
+  end
+  class GradeSerialzer < ActiveModel::Serializer
+    attribute :grade_level
+  end
 end
